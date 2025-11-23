2d9607b4eebabc6ff0039cda0289fc943ca3a552
Kunalgarg92 <kunal.garg2022@vitstudent.ac.in>
2025-11-23 18:43:22 +0530

    initial commit

---
+++ src/main/java/com/chubb/FlightWebfluxAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.chubb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightWebfluxAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightWebfluxAssignmentApplication.class, args);
+	}
+
+}
